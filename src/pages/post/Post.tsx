5bb114a1915b6e34ba0466a61d02ec74824b47f9
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2024-01-16 16:46:40 +0300

    Add Post page layout

--- src/pages/post/Post.tsx
+++ src/pages/post/Post.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Button from "../../shared/button/Button";
+import styles from './Post.module.css';
 
 const Post = () => {
     return (
-        <div>
+        <div className={styles.container}>
+            <div className={styles.info}>
+                <Button>Назад</Button>
+                <p className={styles.attention}>Вы просматриваете пост №1</p>
+            </div>
 
+            <div className={styles.post}>
+                <h1 className={styles.title}>sunt aut facere repellat provident occaecati excepturi optio reprehenderit</h1>
+                <p className={styles.body}>
+                    quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas
+                    totam nostrum rerum est autem sunt rem eveniet architecto
+                </p>
+            </div>
         </div>
     );
 };
