a68d546e023076deacc7c99bb3faf7ab8789d16f
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2024-01-16 17:05:28 +0300

    Add return button

--- src/entities/post/PostCard.tsx
+++ src/entities/post/PostCard.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from 'react';
 import styles from "./PostCard.module.css";
 import Button from "../../shared/button/Button";
+import {Link} from "react-router-dom";
 
 export interface PostCardProps {
     id: number;
@@ -13,7 +14,7 @@ const PostCard: FC<PostCardProps> = ({id, title, body}) => {
         <div className={styles.post}>
             <h2 className={styles.header}>{id}. {title}</h2>
             <p className={styles.text}>{body}</p>
-            <Button>Просмотр</Button>
+            <Link to={`/posts/${id}`}><Button>Просмотр</Button></Link>
         </div>
     );
 };
--- src/pages/post/Post.tsx
+++ src/pages/post/Post.tsx
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Button from "../../shared/button/Button";
 import styles from './Post.module.css';
+import {IPost} from "../posts-list/PostsList";
+import PostService from "../../entities/post/api/PostService";
+import {useNavigate} from "react-router-dom";
+
+export const getPostId = () => {
+    const pathname = window.location.pathname;
+    return Number(pathname.split('/')[2]);
+}
+
 
 const Post = () => {
+    const [post, setPost] = useState<IPost>();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        fetchSpecialist();
+    }, []);
+
+    async function fetchSpecialist () {
+        setTimeout(async () => {
+            try {
+                const response = await PostService.getById(getPostId());
+                setPost(response.data);
+            } catch (err) {
+                console.log(err);
+            }
+        }, )
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.info}>
-                <Button>Назад</Button>
-                <p className={styles.attention}>Вы просматриваете пост №1</p>
+                <Button onClick={() => navigate(-1)}>Назад</Button>
+                <p className={styles.attention}>Вы просматриваете пост №{post?.id}</p>
             </div>
 
             <div className={styles.post}>
-                <h1 className={styles.title}>sunt aut facere repellat provident occaecati excepturi optio reprehenderit</h1>
-                <p className={styles.body}>
-                    quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas
-                    totam nostrum rerum est autem sunt rem eveniet architecto
-                </p>
+                <h1 className={styles.title}>{post?.title}</h1>
+                <p className={styles.body}>{post?.body}</p>
             </div>
         </div>
     );
