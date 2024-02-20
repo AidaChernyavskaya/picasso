435f42f255a808f7933d4059e38b7b08b306e41b
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2024-02-20 16:16:48 +0300

    Add colors to posts cards

--- src/entities/post/PostCard.tsx
+++ src/entities/post/PostCard.tsx
@@ -3,14 +3,20 @@ import styles from "./PostCard.module.css";
 import Button from "../../shared/button/Button";
 import {Link} from "react-router-dom";
 import {IPost} from "../../pages/posts-list/PostsList";
+import cn from 'classnames';
 
 export interface PostCardProps {
     post: IPost;
 }
 
+const getColorById = (id: number): string => {
+    const colors = ['green', 'blue', 'pink', 'purple'];
+    return colors[(id - 1)%4];
+}
+
 const PostCard: FC<PostCardProps> = ({post}) => {
     return (
-        <div className={styles.post}>
+        <div className={cn(styles.post, styles[getColorById(post.id)])}>
             <h2 className={styles.header}>{post.id}. {post.title}</h2>
             <p className={styles.text}>{post.body}</p>
             <Link to={`/posts/${post.id}`}><Button>Просмотр</Button></Link>
